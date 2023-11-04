d456fb09fb5ca79cb8e5232e841917b36842f3e6
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-11-04 04:02:55 +0000

    orgamism作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/searchInnput";
+import { UserCard } from "./components/organisms/user/userCard";
 import "./styles.css";
+
+const user = {
+  name: "岩崎",
+  image:
+    "https://www.livefreely2310.com/my-family/wp-content/uploads/2023/04/ヒロト留年.jpg",
+  email: "123456@example.com",
+  phone: "111-222-333",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://google.com",
+};
 
 export default function App() {
   return (
@@ -10,6 +23,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
